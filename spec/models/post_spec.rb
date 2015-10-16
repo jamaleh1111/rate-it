d9a31bd14da9783b63eb0bb0d82f8eb07c32c489
Jamaleh <Jamaleh@hotmail.com>
2015-10-16 15:28:12 +1100

    add validations for posts and create form partial

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,9 +5,15 @@ RSpec.describe Post, type: :model do
   #creating a parent topic for post
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
-  let(:post) { topic.posts.create!(title: "New Post Title", body: "New Post Body") }
+  let(:post) { topic.posts.create!(title: "New Post Title", body: "New Post Body is 20 characters long") }
 
   it { should belong_to(:topic) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:topic) }
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(20) }
+
 
   describe "attributes" do 
     it "should respond to title" do 
