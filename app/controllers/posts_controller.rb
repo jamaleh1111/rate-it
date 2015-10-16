9fdbcd7044b25f7457d9311feb52d1b5073127d5
Jamaleh <Jamaleh@hotmail.com>
2015-10-16 14:54:32 +1100

    nest post under topics, write specs and controller actions for topic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,15 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
+  #the index of posts are nexted under each topic
+  # def index
+  #   @posts = Post.all
+  # end
 
   def show
     @post = Post.find(params[:id])
   end
 #instantiates a new post
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
 #updates and saves to database.  POST action
@@ -15,10 +17,12 @@ class PostsController < ApplicationController
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id])
+    @post.topic = @topic
 
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was a problem saving the post.  Please try again."
       render :new
@@ -36,9 +40,9 @@ class PostsController < ApplicationController
 
     if @post.save
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@post.topic, @post]
     else 
-      flash[:error] = "There was an error updating the post.  Pleas try again."
+      flash[:error] = "There was an error updating the post.  Please try again."
       render :edit
     end 
   end 
@@ -47,7 +51,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-      redirect_to posts_path
+      redirect_to @post.topic
     else
       flash[:error] = "There was an error deleting the post. Please try again."
       redirect_to :show
