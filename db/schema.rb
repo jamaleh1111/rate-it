e7b2f128f506f8c36a9104842bc9a501e6c1d549
Jamaleh <Jamaleh@hotmail.com>
2015-10-16 11:22:15 +1100

    add topic model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015115505) do
+ActiveRecord::Schema.define(version: 20151016001116) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20151015115505) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
